5a7222a402fc95840afdf36bafa657bb2638c3c2
Mariacarrenonindecardona <202207813@alu.comillas.edu>
2025-02-24 17:05:38 +0100

    avances

---
+++ Sprint2/ttt/app/components/Footer.js
@@ -0,0 +1,11 @@
+// app/components/Footer.js
+export default function Footer() {
+    return (
+      <footer>
+        <p id="footer-home-page">
+          Creado por <strong>Raquel</strong> y <strong>María</strong> - <em>2025</em>
+        </p>
+      </footer>
+    );
+  }
+  
