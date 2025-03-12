8ee22587538b5e2de7178713623c6db0e4ed9ef4
Raquelfes11 <202209790@alu.comillas.edu>
2025-03-12 17:37:17 +0100

    Detalles

--- Sprint2/trabajodasmaria-main/src/components/ProductList/ProductList.js
+++ Sprint2/trabajodasmaria-main/src/components/ProductList/ProductList.js
@@ -1,38 +1,30 @@
 import React from 'react';
-import { Link } from 'react-router-dom'; 
-import styles from "./ProductList.module.css"
+import { Link } from 'react-router-dom';
+import styles from './ProductList.module.css';
 
 function ProductList({ filteredProducts }) {
-
-  const addToCart = (product) => {
-    let cart = JSON.parse(localStorage.getItem("cart")) || []; 
-    cart.push(product); 
-    localStorage.setItem("cart", JSON.stringify(cart)); 
-    alert("Producto añadido al carrito");
-  };
   
   const addToWishlist = (product) => {
-    let wishlist = JSON.parse(localStorage.getItem("wishlist")) || []; 
-    wishlist.push(product); 
-    localStorage.setItem("wishlist", JSON.stringify(wishlist)); 
+    let wishlist = JSON.parse(localStorage.getItem("wishlist")) || [];
+    wishlist.push(product);
+    localStorage.setItem("wishlist", JSON.stringify(wishlist));
     alert("Producto añadido a la wishlist");
   };
 
   return (
-    <div className="product-list">
+    <div className={styles['product-list']}>
       <h2>Lista de Productos</h2>
       {filteredProducts.length > 0 ? (
-        <div className="product-gallery">
+        <div className={styles['product-gallery']}>
           {filteredProducts.map((product) => (
-            <div className="product-card" key={product.id}>
+            <div className={styles['product-card']} key={product.id}>
               <img src={product.thumbnail} alt={product.title} />
               <h3>{product.title}</h3>
               <p>Precio: ${product.price}</p>
               <Link to={`/products/${product.id}`}>
-                <button className="btn-more">Ver más</button>
+                <button className={styles['btn-more']}>Ver más</button>
               </Link>
-              <button className="btn-wishlist" onClick={() => addToWishlist(product)}>Añadir a wishlist</button>
-              <button className="btn-wishlist" onClick={() => addToCart(product)}>Añadir al carrito</button>
+              <button className={styles['btn-wishlist']} onClick={() => addToWishlist(product)}>Añadir a wishlist</button>
             </div>
           ))}
         </div>
@@ -44,3 +36,4 @@ function ProductList({ filteredProducts }) {
 }
 
 export default ProductList;
+
