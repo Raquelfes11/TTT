b4b5e8e0b84d9c3784945d717adc24388ca6015a
Raquelfes11 <202209790@alu.comillas.edu>
2025-04-07 13:00:08 +0200

    .

--- Sprint2/trabajodasmaria-main/src/App.js
+++ Sprint2/trabajodasmaria-main/src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom'; 
+import { Navigate } from 'react-router-dom';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header/Header';
 import HeroMessage from './components/HeroMessage/HeroMessage';
