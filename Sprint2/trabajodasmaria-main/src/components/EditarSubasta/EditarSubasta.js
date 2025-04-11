6d093a925eefab1ba04c1e52ed8095ada41ba80d
Raquelfes11 <202209790@alu.comillas.edu>
2025-04-11 13:10:12 +0200

    Editar subasta

--- Sprint2/trabajodasmaria-main/src/components/EditarSubasta/EditarSubasta.js
+++ Sprint2/trabajodasmaria-main/src/components/EditarSubasta/EditarSubasta.js
@@ -84,7 +84,7 @@ function EditarSubasta({ user }) {
   
       try {
         const response = await fetch(`http://127.0.0.1:8000/api/auctions/${id}/`, {
-          method: 'PUT',
+          method: 'PATCH',
           headers: {
             'Content-Type': 'application/json',
             'Authorization': `Bearer ${accessToken}`,
