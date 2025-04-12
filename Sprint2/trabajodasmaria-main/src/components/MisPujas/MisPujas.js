d90964b75c35c567bef6c1b6828d37a2f6ec4831
Raquelfes11 <202209790@alu.comillas.edu>
2025-04-12 20:48:26 +0200

    Coment

--- Sprint2/trabajodasmaria-main/src/components/MisPujas/MisPujas.js
+++ Sprint2/trabajodasmaria-main/src/components/MisPujas/MisPujas.js
@@ -3,13 +3,15 @@ import styles from './MisPujas.module.css';
 
 function MisPujas({ user, setUser }) {
   const [pujasItems, setPujasItems] = useState([]);
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
 
   useEffect(() => {
     const fetchPujas = async () => {
-      try {
-        const token = localStorage.getItem("accessToken");
+      const token = localStorage.getItem("accessToken");
 
-        if (token) {
+      if (token) {
+        setIsAuthenticated(true); 
+        try {
           const response = await fetch("http://127.0.0.1:8000/api/auctions/misPujas", {
             method: "GET",
             headers: {
@@ -20,41 +22,61 @@ function MisPujas({ user, setUser }) {
 
           if (response.ok) {
             const data = await response.json();
-            setPujasItems(data); // Establece las pujas que el usuario ha realizado
+            setPujasItems(data); 
           } else {
             console.error("Error al obtener las pujas");
           }
-        } else {
-          alert("No estás autenticado");
+        } catch (error) {
+          console.error("Error al hacer la solicitud de pujas:", error);
         }
-      } catch (error) {
-        console.error("Error al hacer la solicitud de pujas:", error);
+      } else {
+        setIsAuthenticated(false); 
       }
     };
 
     fetchPujas();
-  }, []); // El useEffect se ejecuta al cargar el componente
+  }, []); 
 
-  const removeItemFromPujas = (index) => {
-    const updatedPujas = [...pujasItems];
-    updatedPujas.splice(index, 1);
-    setPujasItems(updatedPujas); 
-    localStorage.setItem("misPujas", JSON.stringify(updatedPujas)); 
+  const removeItemFromPujas = async (index, auctionId, bidId) => {
+    const token = localStorage.getItem("accessToken");
+  
+    try {
+      const response = await fetch(`http://127.0.0.1:8000/api/auctions/${auctionId}/bids/${bidId}/`, {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": `Bearer ${token}`,
+        },
+      });
+  
+      if (response.ok) {
+        const updatedPujas = [...pujasItems];
+        updatedPujas.splice(index, 1);
+        setPujasItems(updatedPujas);
+      } else {
+        alert("Error al eliminar la puja");
+      }
+    } catch (error) {
+      console.error("Error al eliminar:", error);
+    }
   };
+  
 
   return (
     <div className={styles.pujas}>
       <h2 className={styles['h2-pujas-page']}>Pujas Realizadas</h2>
-      {pujasItems.length > 0 ? (
+      {!isAuthenticated ? (
+        <p>Te tienes que loguear para ver tus pujas.</p>
+      ) : pujasItems.length > 0 ? (
         <div className={styles['pujas-items']}>
           {pujasItems.map((item, index) => (
             <div className={styles['pujas-item']} key={index}>
-              <img src={item.auction_thumbnail } alt={item.title} />
+              <img src={item.auction_thumbnail} alt={item.auction_title} />
               <h3>{item.auction_title}</h3>
               <p>Cantidad Pujada: ${item.price}</p>
               <button 
                 className={styles['btn-remove']} 
-                onClick={() => removeItemFromPujas(index)}
+                onClick={() => removeItemFromPujas(index, item.auction_id, item.id)}
               >
                 Eliminar
               </button>
@@ -69,5 +91,3 @@ function MisPujas({ user, setUser }) {
 }
 
 export default MisPujas;
-
-
