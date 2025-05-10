7dfcd7355af83f63af8f61b2f1794834f4ac3189
Raquelfes11 <202209790@alu.comillas.edu>
2025-05-10 02:00:12 +0200

    .

--- Sprint2/trabajodasmaria-main/src/components/MisRatings/MisRatings.js
+++ Sprint2/trabajodasmaria-main/src/components/MisRatings/MisRatings.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom'; // Importa Link para la navegación
+import { Link } from 'react-router-dom';
 import styles from './MisRatings.module.css';
 
 function MisRatings() {
